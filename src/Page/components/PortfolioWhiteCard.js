ea5197404dd377284952cce846ec85905a9f9c03
Yulieth Lubo <yuliethdotcom@gmail.com>
2022-02-03 13:45:22 +0100

    Styles fixed

--- src/Page/components/PortfolioWhiteCard.js
+++ src/Page/components/PortfolioWhiteCard.js
@@ -1,15 +1,12 @@
 import React from "react";
 import styled from "styled-components";
+import "../css/Portfolio.css"
 
 const WhiteCardTitle = styled.p`
   color: var(--main-color);
   font-weight: bold;
   font-size: x-large;
   text-align: center;
-  &:hover,
-  &:focus{
-    color: white !important;
-  }
 `;
 
 const WhiteCardText = styled.p`
@@ -18,42 +15,6 @@ const WhiteCardText = styled.p`
   font-size: small;
   text-align: center;
   margin-top: 10px;
-  
-  &:hover,
-  &:focus{
-    color: white !important;
-  }
-`;
-
-const WhiteCardStyled = styled.div`
-  background-color: #fff;
-  border: none;
-  padding: 8px 18px 8px 18px;
-  border-radius: 6px;
-  border: 2px solid #fff;
-  height: 200px;
-  max-width: 250px;
-  display: flex;
-  flex-direction: column;
-  justify-content: center;
-  align-items: center;
-  flex-wrap: wrap;
-  
-  
-  &:hover,
-  &:focus {
-    outline: none;
-    background-color: var(--buttons-color);
-    color: white !important;
-  }
-  box-shadow: 1px 8px 12px rgba(0, 0, 0, 0.1);
-  cursor: pointer;
-  margin: 10px;
-  
-  &:hover,
-  &:focus .p{
-    color: white !important;
-  }
 `;
 
 const ModalButton = styled.button`
@@ -64,23 +25,33 @@ const ModalButton = styled.button`
   padding: 8px 40px 8px 40px;
   border-radius: 6px;
   border: 2px solid var(--buttons-color);
-  &:hover,
-  &:focus {
-    background-color: #fff;
-    color: var(--main-color);
+  opacity: 0;
+  &:hover   {
+    background-color: var(--buttons-color) !important;
+    color: white !important;
     border: 2px solid #fff;
     box-shadow: 1px 8px 12px rgba(0, 0, 0, 0.1);
     cursor: pointer;
+    opacity: 1;
+  }
+  
+  &:focus   {
+    background-color: var(--buttons-color) !important;
+    color: white !important;
+    border: 2px solid #fff;
+    box-shadow: 1px 8px 12px rgba(0, 0, 0, 0.1);
+    cursor: pointer;
+    opacity: 1;
   }
 `;
 
 const WhiteCard = (props) => {
   return (
-    <WhiteCardStyled onClick={props.onClick}>
+    <div className="WhiteCardStyled" onClick={props.onClick}>
       <WhiteCardTitle>{props.Title}</WhiteCardTitle>
       <WhiteCardText>{props.Description}</WhiteCardText>
       <ModalButton>{props.Button}</ModalButton>
-    </WhiteCardStyled>
+    </div>
   );
 };
 

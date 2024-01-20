a6cfd1296114f76e18c4ee62bd77c74c572c622c
Yulieth Lubo <yuliethdotcom@gmail.com>
2024-01-20 18:42:20 +0100

    Update Principal.js

--- src/Page/Principal.js
+++ src/Page/Principal.js
@@ -183,11 +183,7 @@ const Principal = () => {
  
  I'm ready to new challenges, looking for new opportunities.
                   </p>
-                  //<a href={MyCV} download="CV_Yulieth_Lubo.pdf">
-                  //  <StyledButton Title="Download CV"></StyledButton>
-                  //</a><a style={{ marginLeft: "10px" }} href={MyPortfolio} download="Porfolio_Yulieth_Lubo.pdf">
-                   // <StyledButton Title="Download Portfolio"></StyledButton>
-                  //</a>
+             
                 </aboutme>
                 <SkillsSection>
                   <Title>Skills</Title>
